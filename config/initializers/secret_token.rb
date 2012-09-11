0c66db584c92ecd6ba14a4d8e8c095bc03b99894
skywalker <skywalker@skywalker-ubuntu.(none)>
2012-09-11 16:16:20 +0800

    init rails app with some gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LogDashboard::Application.config.secret_token = '329d708ce524334676d0e6d664a754bfa968e7337f0d251a3ee719aecfc1b74545c97834a14a9c155cd3dc315462a0bc47d0c92fde8853686f40b5716b5b4980'
